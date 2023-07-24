c4ff9cdb52a83b9a41217f23a7ad0f5d93e2f3d7
bmxshithead <67148946+weegeedoescoding@users.noreply.github.com>
2023-07-24 18:35:58 +0100

    catthingimajig
    added the nasty blood-borne disease that is my javascript code

---
+++ catspawn.js
@@ -0,0 +1,8 @@
+// Draws the cats, nuff said.
+var drawCats = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + " =^.^=");
+    }
+};
+
+drawCats(10); // any number can go here
